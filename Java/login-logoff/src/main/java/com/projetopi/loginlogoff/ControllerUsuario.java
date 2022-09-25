20a7f4761c26449f2e3d998be4dd172f7df7b393
Costa-E99 <eduardo.costa@bandtec.com.br>
2022-09-25 14:46:34 -0300

    ajustando chamadas http

--- Java/login-logoff/src/main/java/com/projetopi/loginlogoff/ControllerUsuario.java
+++ Java/login-logoff/src/main/java/com/projetopi/loginlogoff/ControllerUsuario.java
@@ -1,5 +1,8 @@
 package com.projetopi.loginlogoff;
 
+import ch.qos.logback.core.pattern.util.RegularEscapeUtil;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -16,7 +19,7 @@ public class ControllerUsuario {
     LocalDateTime hoje = LocalDateTime.now();
 
     @PostMapping
-    public Usuario cadastrarUsuario(@RequestBody Usuario u) {
+    public ResponseEntity<Usuario> cadastrarUsuario(@RequestBody Usuario u) {
         LocalDateTime dataDeNascimento = LocalDateTime.of
                 ((u.getDataNascimento().getYear() + 1900), u.getDataNascimento().getMonth() + 1,
                         u.getDataNascimento().getDate() + 1, 0, 0, 0);
@@ -25,27 +28,27 @@ public class ControllerUsuario {
         if (u.getEmail().contains("@") && u.pegueSenha().length() > 8 && u.getCpf().length() == 11
                 && idade > 15) {
             usuarios.add(u);
-            return u;
+            return ResponseEntity.status(201).body(u);
         }
-        return null;
+        return ResponseEntity.status(404).build();
     }
 
     @GetMapping
-    public List getUsuario( ) {
-            return usuarios;
+    public ResponseEntity<List> getUsuario() {
+            return ResponseEntity.status(202).body(usuarios);
         }
 
 
     @PostMapping ("autenticar/{cpf}/{senha}")
-    public String autenticarUsuario( @PathVariable String cpf, @PathVariable String senha) {
+    public ResponseEntity<String> autenticarUsuario( @PathVariable String cpf, @PathVariable String senha) {
         for (Usuario u : usuarios) {
             if (u.getCpf().toString().equals(cpf) && u.pegueSenha().equals(senha)) {
                 u.setAutenticado(true);
-                return String.format("Login encontrado\n Bem vindo " + u.getNome());
+                return ResponseEntity.status(201).body(String.format("Login realizado com ssucesso "));
             }
         }
 
-        return null;
+        return  ResponseEntity.status(201).body(String.format("Cpf e/ou senha incorretos"));
     }
 }
 
