c05f612bc57d7377551b49feaa3d6a5197c08e50
nataliasiqueira <ssiqueira.natalia@gmail.com>
2022-11-02 21:52:10 -0300

    Gravação de Log dos endpoints de Despesa

--- Java/login-logoff/src/main/java/com/projetopi/loginlogoff/financas/despesa/DespesaController.java
+++ Java/login-logoff/src/main/java/com/projetopi/loginlogoff/financas/despesa/DespesaController.java
@@ -1,8 +1,12 @@
 package com.projetopi.loginlogoff.financas.despesa;
 
+import com.projetopi.loginlogoff.Log;
 import com.projetopi.loginlogoff.usuario.Usuario;
 import com.projetopi.loginlogoff.usuario.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,6 +25,8 @@ public class DespesaController {
     @Autowired
     DespesaRepository despesaRepository;
 
+    Log log = new Log();
+
     @GetMapping("/{idUsuario}")
     public ResponseEntity<List<Despesa>> listarTodasDespesasDoUsuario(@PathVariable int idUsuario) {
         if (usuarioRepository.existsById(idUsuario)) {
@@ -32,11 +38,26 @@ public class DespesaController {
                 }
             }
             if (todasDespesasDoUsuario.isEmpty()) {
-                return ResponseEntity.status(204).build();
+                ResponseEntity respostaVazio = ResponseEntity.status(204).build();
+                String statusCode = respostaVazio.getStatusCode().toString();
+                String logResposta = respostaVazio.getStatusCode().series().toString();
+                String textoLog = "\nENDPOINT: listarTodasDespesasDoUsuario \nStatus Code: " + statusCode + "\nLog: " + logResposta + "\nidUsuario: " + idUsuario;
+                log.gravaLog(textoLog);
+                return respostaVazio;
             }
-            return ResponseEntity.status(200).body(todasDespesasDoUsuario);
+            ResponseEntity respostaOk = ResponseEntity.status(200).body(todasDespesasDoUsuario);
+            String statusCode = respostaOk.getStatusCode().toString();
+            String logResposta = respostaOk.getStatusCode().series().toString();
+            String textoLog = "\nENDPOINT: listarTodasDespesasDoUsuario \nStatus Code: " + statusCode + "\nLog: " + logResposta + "\nidUsuário: " + idUsuario;
+            log.gravaLog(textoLog);
+            return respostaOk;
         }
-        return ResponseEntity.status(404).build();
+        ResponseEntity respostaErro = ResponseEntity.status(404).build();
+        String statusCode = respostaErro.getStatusCode().toString();
+        String logResposta = respostaErro.getStatusCode().series().toString();
+        String textoLog = "\nENDPOINT: listarTodasDespesasDoUsuario \nStatus Code: " + statusCode + "\nLog: " + logResposta + "\nidUsuário: " + idUsuario;
+        log.gravaLog(textoLog);
+        return respostaErro;
     }
 
     @GetMapping("/{idUsuario}/{idDespesa}")
@@ -45,12 +66,27 @@ public class DespesaController {
             List<Despesa> todasDespesas = despesaRepository.findAll();
             for (Despesa despesaAtual : todasDespesas) {
                 if (despesaAtual.getFkUsuario() == idUsuario && despesaAtual.getCodigo() == idDespesa) {
-                    return ResponseEntity.status(200).body(despesaAtual);
+                    ResponseEntity respostaOk = ResponseEntity.status(200).body(despesaAtual);
+                    String statusCode = respostaOk.getStatusCode().toString();
+                    String logResposta = respostaOk.getStatusCode().series().toString();
+                    String textoLog = "\nENDPOINT: listarTodasDespesasDoUsuario \nStatus Code: " + statusCode + "\nLog: " + logResposta + "\nidUsuário: " + idUsuario;
+                    log.gravaLog(textoLog);
+                    return respostaOk;
                 }
             }
-            return ResponseEntity.status(204).build();
+            ResponseEntity respostaVazio = ResponseEntity.status(204).build();
+            String statusCode = respostaVazio.getStatusCode().toString();
+            String logResposta = respostaVazio.getStatusCode().series().toString();
+            String textoLog = "\nENDPOINT: listarTodasDespesasDoUsuario \nStatus Code: " + statusCode + "\nLog: " + logResposta + "idUsuario: " + idUsuario;
+            log.gravaLog(textoLog);
+            return respostaVazio;
         }
-        return ResponseEntity.status(404).build();
+        ResponseEntity respostaErro = ResponseEntity.status(404).build();
+        String statusCode = respostaErro.getStatusCode().toString();
+        String logResposta = respostaErro.getStatusCode().series().toString();
+        String textoLog = "\nENDPOINT: listarTodasDespesasDoUsuario \nStatus Code: " + statusCode + "\nLog: " + logResposta + "\nidUsuário: " + idUsuario;
+        log.gravaLog(textoLog);
+        return respostaErro;
     }
 
     @PostMapping("/{idUsuario}")
@@ -58,9 +94,19 @@ public class DespesaController {
         if (usuarioRepository.existsById(idUsuario)) {
             Optional<Usuario> usuario = usuarioRepository.findById(idUsuario);
             despesa.setUsuario(usuario.get());
-            return ResponseEntity.status(201).body(despesaRepository.save(despesa));
+            ResponseEntity respostaOk = ResponseEntity.status(201).body(despesaRepository.save(despesa));
+            String statusCode = respostaOk.getStatusCode().toString();
+            String logResposta = respostaOk.getStatusCode().series().toString();
+            String textoLog = "\nENDPOINT: criarDespesa \nStatus Code: " + statusCode + "\nLog: " + logResposta + "\nidUsuário: " + idUsuario;
+            log.gravaLog(textoLog);
+            return respostaOk;
         }
-        return ResponseEntity.status(404).build();
+        ResponseEntity respostaErro = ResponseEntity.status(404).build();
+        String statusCode = respostaErro.getStatusCode().toString();
+        String logResposta = respostaErro.getStatusCode().series().toString();
+        String textoLog = "\nENDPOINT: criarDespesa \nStatus Code: " + statusCode + "\nLog: " + logResposta + "\nidUsuário: " + idUsuario;
+        log.gravaLog(textoLog);
+        return respostaErro;
     }
 
     @PutMapping("/{idUsuario}/{idDespesa}")
@@ -79,9 +125,19 @@ public class DespesaController {
             );
             Optional<Usuario> usuario = usuarioRepository.findById(idUsuario);
             despesaAtualizada.setUsuario(usuario.get());
-            return ResponseEntity.status(200).body(despesaRepository.save(despesaAtualizada));
+            ResponseEntity respostaOk = ResponseEntity.status(200).body(despesaRepository.save(despesaAtualizada));
+            String statusCode = respostaOk.getStatusCode().toString();
+            String logResposta = respostaOk.getStatusCode().series().toString();
+            String textoLog = "\nENDPOINT: atualizarDespesa \nStatus Code: " + statusCode + "\nLog: " + logResposta + "\nidUsuário: " + idUsuario;
+            log.gravaLog(textoLog);
+            return respostaOk;
         }
-        return ResponseEntity.status(404).build();
+        ResponseEntity respostaErro = ResponseEntity.status(404).build();
+        String statusCode = respostaErro.getStatusCode().toString();
+        String logResposta = respostaErro.getStatusCode().series().toString();
+        String textoLog = "\nENDPOINT: atualizarDespesa \nStatus Code: " + statusCode + "\nLog: " + logResposta + "\nidUsuário: " + idUsuario;
+        log.gravaLog(textoLog);
+        return respostaErro;
     }
 
     @DeleteMapping("/{idUsuario}/{idDespesa}")
@@ -89,15 +145,19 @@ public class DespesaController {
         if (usuarioRepository.existsById(idUsuario) && despesaRepository.existsById(idDespesa)) {
             Optional<Despesa> despesa = despesaRepository.findById(idDespesa);
             despesaRepository.deleteById(idDespesa);
-            ResponseEntity resposta = ResponseEntity.status(200).body(despesa.get());
-            String erro = resposta.getStatusCode().getReasonPhrase();
-            System.out.println(erro);
-            return resposta;
+            ResponseEntity respostaOk = ResponseEntity.status(200).body(despesa.get());
+            String statusCode = respostaOk.getStatusCode().toString();
+            String logResposta = respostaOk.getStatusCode().series().toString();
+            String textoLog = "\nENDPOINT: deletarDespesa \nStatus Code: " + statusCode + "\nLog: " + logResposta + "\nidUsuário: " + idUsuario;
+            log.gravaLog(textoLog);
+            return respostaOk;
         }
-        ResponseEntity resposta2 = ResponseEntity.status(404).build();
-        String erro2 = resposta2.getStatusCode().getReasonPhrase();
-        System.out.printf("SOUT AQUI: %s IDUSUARIO: %d", erro2, idUsuario);
-        return resposta2;
+        ResponseEntity respostaErro = ResponseEntity.status(404).build();
+        String statusCode = respostaErro.getStatusCode().toString();
+        String logResposta = respostaErro.getStatusCode().series().toString();
+        String textoLog = "\nENDPOINT: deletarDespesa \nStatus Code: " + statusCode + "\nLog: " + logResposta + "\nidUsuário: " + idUsuario;
+        log.gravaLog(textoLog);
+        return respostaErro;
     }
 
     @DeleteMapping("/deletarTodas/{idDespesa}")
@@ -112,11 +172,25 @@ public class DespesaController {
                 }
             }
             if (todasDespesassDeletadas.size() == 0) {
-                return ResponseEntity.status(202).build();
+                ResponseEntity respostaVazio = ResponseEntity.status(202).build();
+                String statusCode = respostaVazio.getStatusCode().toString();
+                String logResposta = respostaVazio.getStatusCode().series().toString();
+                String textoLog = "\nENDPOINT: deletarTodasDespesas \nStatus Code: " + statusCode + "\nLog: " + logResposta;
+                log.gravaLog(textoLog);
+                return respostaVazio;
             }
-            return ResponseEntity.status(200).body(todasDespesassDeletadas);
+            ResponseEntity respostaOk = ResponseEntity.status(200).body(todasDespesassDeletadas);
+            String statusCode = respostaOk.getStatusCode().toString();
+            String logResposta = respostaOk.getStatusCode().series().toString();
+            String textoLog = "\nENDPOINT: deletarTodasDespesas \nStatus Code: " + statusCode + "\nLog: " + logResposta;
+            log.gravaLog(textoLog);
+            return respostaOk;
         }
-        return ResponseEntity.status(404).build();
+        ResponseEntity respostaErro = ResponseEntity.status(404).build();
+        String statusCode = respostaErro.getStatusCode().toString();
+        String logResposta = respostaErro.getStatusCode().series().toString();
+        String textoLog = "\nENDPOINT: deletarTodasDespesas \nStatus Code: " + statusCode + "\nLog: " + logResposta;
+        log.gravaLog(textoLog);
+        return respostaErro;
     }
-
 }
