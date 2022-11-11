0a5c4845b8908a9e89c8dc7d95c7f5be4ba0f3a6
mtorokm <MATHEUS.TOROK@bandtec.com.br>
2022-11-10 21:21:08 -0300

    dto despesas

--- Java/login-logoff/src/main/java/com/projetopi/loginlogoff/financas/despesa/DespesaRepository.java
+++ Java/login-logoff/src/main/java/com/projetopi/loginlogoff/financas/despesa/DespesaRepository.java
@@ -1,13 +1,18 @@
 package com.projetopi.loginlogoff.financas.despesa;
 
+import com.projetopi.loginlogoff.financas.despesa.dto.DespesaDto;
 import com.projetopi.loginlogoff.financas.objetivo.Objetivo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface DespesaRepository extends JpaRepository<Despesa, Integer> {
     List<Despesa> findByUsuarioIdOrderByData(int idUsuario);
-
     int countByUsuarioId(int idUsuario);
 
+    @Query("SELECT" +
+            " new com.projetopi.loginlogoff.financas.despesa.dto.DespesaDto(d.codigo, d.nome, d.descricao, d.valor)" +
+            " FROM Despesa d where usuario_id = ?1")
+    List<DespesaDto> getDespesaDto(int fkUsuario);
 }
