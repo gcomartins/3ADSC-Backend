b533fd8d6d2687bf946616d49d62751dd755e5e7
costa-E77 <eduardo.ecosta@linx.com.br>
2023-05-31 19:01:41 -0300

    implementando banco na azure

--- Java/login-logoff/src/test/java/com/projetopi/loginlogoff/LoginLogoffApplicationTests.java
+++ Java/login-logoff/src/test/java/com/projetopi/loginlogoff/LoginLogoffApplicationTests.java
@@ -1,13 +1,13 @@
-package com.projetopi.loginlogoff;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class LoginLogoffApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.projetopi.loginlogoff;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class LoginLogoffApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
